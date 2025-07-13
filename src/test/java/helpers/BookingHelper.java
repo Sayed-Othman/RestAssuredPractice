92c9c5e7dbaa2094f0e6536e5642741aff2b8e83
Sayed Othman <sothman@eservicios.indracompany.com>
2025-07-13 16:21:46 +0300

    change the booking client logic

--- src/test/java/helpers/BookingHelper.java
+++ src/test/java/helpers/BookingHelper.java
@@ -1,26 +1,22 @@
 package helpers;
 
-import clients.BookingClient;
+import clients.RestAssuredClient;
 import config.APIConstant;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import models.request.BookingRequest;
 import utils.JsonReader;
 
 public class BookingHelper {
 
-    private final BookingClient client = new BookingClient(APIConstant.BASE_URL);
+    private final RestAssuredClient client = new RestAssuredClient(APIConstant.BASE_URL);
 
-    /**
-     * Loads booking request data from a JSON file.
-     *
-     * @return BookingRequest object loaded from booking.json
-     */
     public BookingRequest defaultRequest() {
         String jsonFilePath = "src/test/resources/booking.json";
         return JsonReader.readJson(jsonFilePath, BookingRequest.class);
     }
 
     public Response createBooking(BookingRequest request) {
-        return client.createBooking(APIConstant.Booking.CREATE_BOOKING, request);
+        return client.postJson(APIConstant.Booking.CREATE_BOOKING, null, request, ContentType.JSON);
     }
 }
